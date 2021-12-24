4e7b730e1a056c7feb1e9b5ead8cb08fa1f8f131
Raiwin Inthasit <andaraiwin@gmail.com>
2021-12-24 13:52:06 +0700

    Lab13_1 Completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    int y = x;
+    if(x == 0){
+        y = 0;
+    }else if(x == 1){
+        y = 1;
+    }else{
+        y = fibonacci(x - 1) + fibonacci(x - 2);
+    }
+    return y;
 }
