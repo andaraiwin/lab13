b5a2feb86422409500b6b9b72405fb3a1ab49225
Raiwin Inthasit <andaraiwin@gmail.com>
2021-12-24 15:38:32 +0700

    lab13_2 Completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int , int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x, int y){
+    int a,b;
+    if(x%y == 0){
+        return y;
+    }else if(y%x == 0){
+        return x;
+    }else{
+        a = gcd(y, x%y);
+        return a;
+    }
 }
